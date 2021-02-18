daf0fba77c0f729e599b61a8b1838ff80c2012e7
Fwerf <afs1424ad@gmail.com>
2021-02-19 01:02:38 +0300

    fixed no pageobject for web part of task

--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,9 +1,15 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.*;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class WebPageObject  {
+
+    @AndroidFindBy(xpath = "//android.view.View[@text='www.epam.com']")
+    @iOSXCUITFindBy(xpath = "//*[@text='www.epam.com]")
+    private RemoteWebElement result;
 
     public WebPageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
