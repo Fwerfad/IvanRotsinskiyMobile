2641b35e33c226c8b39f1ab9a719779d75b56138
Fwerf <afs1424ad@gmail.com>
2021-02-21 21:29:45 +0300

    -

--- src/main/java/pageObjects/WebPageObject.java
+++ src/main/java/pageObjects/WebPageObject.java
@@ -1,14 +1,14 @@
 package pageObjects;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WebPageObject  {
+public class WebPageObject {
 
-    @FindBy(id = "rso")
-    private RemoteWebElement result;
+    @FindBy(xpath = "//*[@id='rso'")
+    private WebElement result;
 
     public WebPageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(appiumDriver, this);
