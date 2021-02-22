68f5c500522af4395066a41a71c6f8ce887c779f
Fwerf <afs1424ad@gmail.com>
2021-02-22 15:00:04 +0300

    -

--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -1,13 +1,10 @@
 package scenarios;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import setup.BaseTest;
-
-import java.util.Objects;
 
 public class nativeMobileTests extends BaseTest {
 
